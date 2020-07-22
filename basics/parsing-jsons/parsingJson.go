c2858e0f18796c0ce896b82160770c4c60e2301c
lethienhoang <45428011+lethienhoang@users.noreply.github.com>
2020-07-22 19:43:16 +0700

    how to read file json and parse them into a struct

---
+++ basics/parsing-jsons/parsingJson.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+type Users struct {
+	Users []User `json:"users"`
+}
+
+type User struct {
+	Name   string `json:"name`
+	Type   string `json:"type`
+	Age    int    `json:Age`
+	Social Social `json:"social"`
+}
+
+type Social struct {
+	Facebook string `json:"facebook"`
+	Twitter  string `json:"twitter"`
+}
+
+func main() {
+	// read file into memory
+	jsonFile, err := os.Open("users.json")
+
+	// check error
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully opened user.json")
+
+	// convert jsonFile to by array
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// we initialize our Users array
+
+	var users Users
+
+	json.Unmarshal(byteValue, &users)
+
+	for i := 0; i < len(users.Users); i++ {
+		fmt.Println("User Type: " + users.Users[i].Type)
+		fmt.Println("User Age: " + strconv.Itoa(users.Users[i].Age))
+		fmt.Println("User Name: " + users.Users[i].Name)
+		fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
+	}
+
+	// defer statements delay the execution of the function or method or an anonymous method until the nearby functions returns
+	defer jsonFile.Close()
+
+}
