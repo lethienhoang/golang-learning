c4b2308d88d976e1ee75594e41a72e20b0764813
lethienhoang <45428011+lethienhoang@users.noreply.github.com>
2020-08-01 13:51:59 +0700

    Unit test done, apply router

--- advanced/building-and-testing-restapi-mux-postgresql/main_test.go
+++ advanced/building-and-testing-restapi-mux-postgresql/main_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/joho/godotenv"
@@ -12,12 +16,12 @@ import (
 
 var app = App{}
 
-const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS product
 (
     id SERIAL,
     name TEXT NOT NULL,
     price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
-    CONSTRAINT products_pkey PRIMARY KEY (id)
+    CONSTRAINT product_pkey PRIMARY KEY (id)
 )`
 
 func TestMain(m *testing.M) {
@@ -45,21 +49,119 @@ func ensureTableExists() {
 }
 
 func clearTable() {
-	app.DB.Exec("DELETE FROM Products")
-	app.DB.Exec("ALTER SEQUENCE Products_id_seq RESTART WITH 1")
+	app.DB.Exec("DELETE FROM Product")
+	app.DB.Exec("ALTER SEQUENCE Product_id_seq RESTART WITH 1")
 }
 
-func TestEmptyTable(t *testing.T) {
+func TestGetNonExistentProduct(t *testing.T) {
 	clearTable()
 
-	req, _ := http.NewRequest("GET", "/products", nil)
+	req, _ := http.NewRequest("GET", "/product/11", nil)
 	res := executeRequest(req)
 
-	checkResponseCode(t, http.StatusOK, res.Code)
+	checkResponseCode(t, http.StatusNotFound, res.Code)
 
-	if body := res.Body.String(); body != "[]" {
-		t.Errorf("Expected an empty array. Got %s", body)
+	var m map[string]string
+	json.Unmarshal(res.Body.Bytes(), &m)
+	if m["error"] != "Product not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'Product not found'. Got '%s'", m["error"])
 	}
+}
+
+func TestCreateProduct(t *testing.T) {
+
+	clearTable()
+
+	var jsonStr = []byte(`{"name":"test product", "price": 11.22}`)
+	req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusCreated, response.Code)
+
+	var m map[string]interface{} // is a map whose keys are strings and values are any type.
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	fmt.Println(m)
+	if m["name"] != "test product" {
+		t.Errorf("Expected product name to be 'test product'. Got '%v'", m["name"])
+	}
+
+	if m["price"] != 11.22 {
+		t.Errorf("Expected product price to be '11.22'. Got '%v'", m["price"])
+	}
+
+	if m["id"] != 1.0 {
+		t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
+	}
+}
+
+func addProducts(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		app.DB.Exec("INSERT INTO product(name, price) VALUES($1, $2)", "Product "+strconv.Itoa(i), (i+1.0)*10)
+	}
+}
+
+func TestGetProduct(t *testing.T) {
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func TestUpdateProduct(t *testing.T) {
+
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := executeRequest(req)
+	var originalProduct map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &originalProduct)
+
+	var jsonStr = []byte(`{"name":"test product - updated name", "price": 11.22}`)
+	req, _ = http.NewRequest("PUT", "/product/1", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] == originalProduct["name"] {
+		t.Errorf("Expected the name to change from '%v' to '%v'. Got '%v'", originalProduct["name"], m["name"], m["name"])
+	}
+
+	if m["price"] == originalProduct["price"] {
+		t.Errorf("Expected the price to change from '%v' to '%v'. Got '%v'", originalProduct["price"], m["price"], m["price"])
+	}
+}
+
+func TestDeleteProduct(t *testing.T) {
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("DELETE", "/product/1", nil)
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("GET", "/product/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
 }
 
 func executeRequest(req *http.Request) *httptest.ResponseRecorder {
--- advanced/building-and-testing-restapi-mux-postgresql/model.go
+++ advanced/building-and-testing-restapi-mux-postgresql/model.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type Product struct {
@@ -11,22 +10,51 @@ type Product struct {
 	Price float64 `json:"price"`
 }
 
-func (p *Product) getProducts(db *sql.DB, start, limit int) error {
-	return errors.New("Not implemented")
+func getProducts(db *sql.DB, start, limit int) ([]Product, error) {
+	rows, err := db.Query("SELECT id, name, price FROM product LIMIT $1 OFFSET $2", limit, start)
+
+	if err == nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	products := []Product{}
+
+	for rows.Next() {
+		var p Product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+
+		products = append(products, p)
+	}
+
+	return products, nil
 }
 
 func (p *Product) getProduct(db *sql.DB, id int) error {
-	return errors.New("Not implemented")
+	return db.QueryRow("SELECT name, price FROM product WHERE id=$1", id).Scan(&p.Name, &p.Price)
 }
 
 func (p *Product) updateProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	_, err :=
+		db.Exec("UPDATE product SET name=$1, price=$2 WHERE id=$3",
+			p.Name, p.Price, p.ID)
+
+	return err
 }
 
-func (p *Product) deleteProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (p *Product) deleteProduct(db *sql.DB, id int) error {
+	_, err := db.Exec("DELETE FROM product WHERE id=$1", id)
+	return err
 }
 
 func (p *Product) createProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	err := db.QueryRow("INSERT INTO product(name, price) VALUES($1, $2) RETURNING id", p.Name, p.Price).Scan(&p.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
